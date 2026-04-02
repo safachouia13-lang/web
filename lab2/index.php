5ae3dcadb112e4af6350d828bdab02d37c98ade2
safachouia13-lang <safachouia13@gmail.com>
2026-04-02 20:33:07 +0100

    Add initial HTML structure for GPA calculator

---
+++ lab2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <form method="post" action="calculate.php" onsubmit="return validateForm()">
+<br><br>
+<div id="courses">
+
+    <input type="text" name="course[]">
+    <input type="number" name="credits[]">
+    <select name="grade[]">
+        <option value="4">A</option>
+        <option value="3">B</option>
+        <option value="2">C</option>
+        <option value="1">D</option>
+        <option value="0">F</option>
+    </select>
+
+</div>
+<button type="button" onclick="addCourse()">Add Course</button>
+<input type="submit" value="Calculate GPA">
+</form>
+  </body>
+</html>
