4707349eed0aeef8f7bdd2714dde0346e9d600fc
safachouia13-lang <safachouia13@gmail.com>
2026-04-02 21:04:40 +0100

    Add GPA calculation and distinction display
    Calculate GPA based on submitted credits and grades, and display distinction level.

--- lab2/index.php
+++ lab2/index.php
@@ -26,3 +26,31 @@
 </form>
   </body>
 </html>
+<?php
+
+$credits = $_POST["credits"];
+$grades = $_POST["grade"];
+
+$totalPoints = 0;
+$totalCredits = 0;
+
+for ($i = 0; $i < count($credits); $i++) {
+    $totalPoints += $credits[$i] * $grades[$i];
+    $totalCredits += $credits[$i];
+}
+
+$gpa = $totalPoints / $totalCredits;
+
+echo "Your GPA is: " . $gpa . "<br>";
+
+if ($gpa >= 3.7) {
+    echo "Distinction";
+} elseif ($gpa >= 3.0) {
+    echo "Merit";
+} elseif ($gpa >= 2.0) {
+    echo "Pass";
+} else {
+    echo "Fail";
+}
+
+?>
