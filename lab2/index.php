0e8229ed9ea8af54a1a172afe383f5772f29cdef
safachouia13-lang <safachouia13@gmail.com>
2026-04-08 18:52:28 +0100

    Update index.php

--- lab2/index.php
+++ lab2/index.php
@@ -1,18 +1,61 @@
+<?php
+$gpa = "";
+$result = "";
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $course = $_POST["course"];
+    $credits = $_POST["credits"];
+    $grades = $_POST["grade"];
+
+    $totalPoints = 0;
+    $totalCredits = 0;
+    $isEmpty = true;
+
+    for ($i = 0; $i < count($credits); $i++) {
+        if (!empty($credits[$i]) && $grades[$i] !== "") {
+            $isEmpty = false;
+            $totalPoints += $credits[$i] * $grades[$i];
+            $totalCredits += $credits[$i];
+        }
+    }
+
+    if ($isEmpty || $totalCredits == 0) {
+        $error = "There is no information!!";
+    } else {
+        $gpa = $totalPoints / $totalCredits;
+
+        if ($gpa >= 3.7) {
+            $result = "Distinction";
+        } elseif ($gpa >= 3.0) {
+            $result = "Merit";
+        } elseif ($gpa >= 2.0) {
+            $result = "Pass";
+        } else {
+            $result = "Fail";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <title></title>
-    <script src="script.js"></script>
+<head>
+    <title>GPA Calculator</title>
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-      <form method="post" onsubmit="return validateForm()">
-<br><br>
+</head>
+<body>
+
+<h1>GPA Calculator</h1>
+
+<form method="post">
 <div id="courses">
 
-    <input type="text" name="course[]">
-    <input type="number" name="credits[]">
+    <input type="text" name="course[]" placeholder="Course Name">
+    <input type="number" name="credits[]" placeholder="Credits" min="1" max="5">
+
     <select name="grade[]">
+        <option value="">Select Grade</option>
         <option value="4">A</option>
         <option value="3">B</option>
         <option value="2">C</option>
@@ -21,36 +64,23 @@
     </select>
 
 </div>
-<button type="button" onclick="addCourse()">Add Course</button>
-<input type="submit" value="Calculate GPA">
+
+<br><br>
+<input type="submit" value="Calculate">
 </form>
-  </body>
-</html>
+
+<br>
+
 <?php
-
-$credits = $_POST["credits"];
-$grades = $_POST["grade"];
-
-$totalPoints = 0;
-$totalCredits = 0;
-
-for ($i = 0; $i < count($credits); $i++) {
-    $totalPoints += $credits[$i] * $grades[$i];
-    $totalCredits += $credits[$i];
+if ($error != "") {
+    echo "<p>$error</p>";
 }
 
-$gpa = $totalPoints / $totalCredits;
-
-echo "Your GPA is: " . $gpa . "<br>";
-
-if ($gpa >= 3.7) {
-    echo "Distinction";
-} elseif ($gpa >= 3.0) {
-    echo "Merit";
-} elseif ($gpa >= 2.0) {
-    echo "Pass";
-} else {
-    echo "Fail";
+if ($gpa != "") {
+    echo "<h3>Your GPA is: " . number_format($gpa,2) . "</h3>";
+    echo "<h4>$result</h4>";
 }
-
 ?>
+
+</body>
+</html>
